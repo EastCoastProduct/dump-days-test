1b6c3445d32316134126c1877a12981fec85b854
Ivan Blazevic <ivanblazevic17@gmail.com>
2016-05-31 15:39:30 +0200

    little more going on

--- index.js
+++ index.js
@@ -2,14 +2,39 @@ const app = require('express')();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const database = require('./database');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/login', function(req, res) {
+  if (!req.body || !req.body.name) {
+    return res.status(400).json({message: 'Name required'});
+  }
+
+  if (database.indexOf(req.body.name) !== -1) {
+    return res.status(400).json({message: 'Name already taken'});
+  }
+
+  database.push(req.body.name);
+
+  res.status(200).json({message: 'success'});
+});
 
 app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', `${msg.author}: ${msg.message}`);
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit(
+      'chat message',
+      {
+        author: msg.author,
+        message: `${msg.author}: ${msg.message}`
+      }
+    );
   });
 });
 
